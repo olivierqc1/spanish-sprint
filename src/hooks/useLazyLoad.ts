ea03dcd848c528b1766f40287c3bca4b56332339
olivierqc1 <olivierturgeon@hotmail.fr>
2025-11-11 14:29:20 +0100

    Create useLazyLoad.ts

---
+++ src/hooks/useLazyLoad.ts
@@ -0,0 +1,37 @@
+// src/hooks/useLazyLoad.ts
+import { useEffect, useRef, useState } from 'react';
+
+export function useLazyLoad<T extends HTMLElement>(
+  options?: IntersectionObserverInit
+) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const ref = useRef<T>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsIntersecting(true);
+          observer.disconnect();
+        }
+      },
+      {
+        rootMargin: '50px',
+        ...options,
+      }
+    );
+
+    const currentRef = ref.current;
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [options]);
+
+  return { ref, isIntersecting };
+}
