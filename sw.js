e1807301742369fd654a983392dd8fdeeb704e24
olivierqc1 <olivierturgeon@hotmail.fr>
2025-12-04 15:52:49 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = 'spanish-sprint-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/grammar',
+  '/flashcards',
+  '/conversations',
+  '/conjugation',
+  '/listen',
+  '/reading'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  if (event.request.url.startsWith('chrome-extension://')) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request).then((response) => {
+          return response || caches.match('/');
+        });
+      })
+  );
+});
