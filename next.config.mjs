d2e0d192b64f382a30c284eaa08b7c1e19709ad1
olivierqc1 <olivierturgeon@hotmail.fr>
2025-12-04 15:54:48 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = { 
+  reactStrictMode: true,
+  // PWA configuration
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+        ],
+      },
+    ];
+  },
+};
+
 export default nextConfig;
