281515cb79a50daf579ca72840a777f98422297c
olivierqc1 <olivierturgeon@hotmail.fr>
2025-12-21 17:02:21 +0100

    Crear index.ts

---
+++ src/data/words/index.ts
@@ -0,0 +1,10 @@
+// src/data/words/index.ts
+
+import { A1Words } from './A1';
+import { A2Words } from './A2';
+import { B1Words } from './B1';
+
+export const flashcardsWords = {
+  A1: A1Words,
+  A2: A2Words
+};
