2cba9b2b63b4981440be183ca65435ad4f2c5145
olivierqc1 <olivierturgeon@hotmail.fr>
2026-04-04 15:12:36 +0200

    Actualizar index.ts

--- src/data/words/index.ts
+++ src/data/words/index.ts
@@ -1,11 +1,58 @@
-// src/data/words/index.ts
+import type { ReadingItem } from "./culturalTexts";
+import { spainA1, spainA2Enriched, mexicoA1Enriched, mexicoA2Enriched } from "./spainA1";
+import { spainA2 } from "./spainA2";
+import { spainB1 } from "./spainB1";
+import { mexicoA1 } from "./mexicoA1";
+import { mexicoA2 } from "./mexicoA2";
+import { mexicoB1 } from "./mexicoB1";
+import { colombiaA1, colombiaA2, colombiaB1 } from "./colombia";
+import { peruA1, peruA2, peruB1 } from "./peru";
+import { chileA1, chileA2, chileB1 } from "./chile";
+import { argentinaA1, argentinaA2, argentinaB1 } from "./argentinaTexts";
+import { cubaA1, cubaA2, cubaB1 } from "./cubaTexts";
+import { venezuelaA1, venezuelaA2, venezuelaB1 } from "./venezuelaTexts";
 
-import { wordsA1 } from './A1';
-import { wordsA2 } from './A2';
-import { wordsB1 } from './B1';
+export const allTexts: ReadingItem[] = [
+  // Espagne
+  ...spainA1,
+  ...spainA2Enriched,
+  ...spainA2,
+  ...spainB1,
 
-export const flashcardsWords = {
-  A1: wordsA1,
-  A2: wordsA2,
-  B1: wordsB1
-};
+  // Mexique
+  ...mexicoA1Enriched,
+  ...mexicoA1,
+  ...mexicoA2Enriched,
+  ...mexicoA2,
+  ...mexicoB1,
+
+  // Colombie
+  ...colombiaA1,
+  ...colombiaA2,
+  ...colombiaB1,
+
+  // Pérou
+  ...peruA1,
+  ...peruA2,
+  ...peruB1,
+
+  // Chili
+  ...chileA1,
+  ...chileA2,
+  ...chileB1,
+
+  // Argentine
+  ...argentinaA1,
+  ...argentinaA2,
+  ...argentinaB1,
+
+  // Cuba
+  ...cubaA1,
+  ...cubaA2,
+  ...cubaB1,
+
+  // Venezuela
+  ...venezuelaA1,
+  ...venezuelaA2,
+  ...venezuelaB1,
+];
